514b1b4c7ca3b76f13282f4601cfdd5c273d1d68
ちあき <chiaki@chiakinoMacBook-Air.local>
2024-05-28 18:46:54 +0900

    商品一覧画面_表作成

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'company_name',
+        'street_address',
+        'representative_name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public static function getCompaniesQuery()
+    {     
+        return DB::table('companies')->get();
+    }
+
+    public function getManufacturers()
+    {
+        return DB::table('companies')->pluck('company_name', 'id');
+    }
 }
